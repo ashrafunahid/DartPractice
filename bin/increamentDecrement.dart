57687d96a7ff9449a6574781e50a998d1b1714e4
Ashraf Uddin <ashrafunahid@gmail.com>
2021-09-28 12:09:26 +0600

    Increment Decrement Practice Added

---
+++ bin/increamentDecrement.dart
@@ -0,0 +1,20 @@
+void main(){
+  int x = 10;
+
+  // Post Increment
+  print(x++);
+  print(x);
+
+  // Pre Increment
+  print(++x);
+  print(x);
+
+  // Post Decrement
+  print(x--);
+  print(x);
+
+  // Pre Decrement
+  print(--x);
+  print(x);
+}
+
