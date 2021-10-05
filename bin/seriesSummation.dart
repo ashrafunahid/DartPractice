0b6315bc1da004141208e90cac4e4bcb5c3b4cb0
Ashraf Uddin <ashrafunahid@gmail.com>
2021-10-05 11:50:24 +0600

    Updated series summation

--- bin/seriesSummation.dart
+++ bin/seriesSummation.dart
@@ -4,4 +4,20 @@ void main() {
     sum = i+sum;
   }
   print(sum);
+
+  //   int i = 0, sum = 0;
+  // while(i <= 100){
+  //
+  //   sum = sum + i;
+  //   i++;
+  //
+  // }
+  // print(sum);
+  //
+  // do{
+  //   sum = sum + i;
+  //   i++;
+  // }
+  // while(i <= 100);
+  // print(sum);
 }
