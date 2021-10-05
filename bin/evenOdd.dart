ffe4ca06c21c26886534b7b4df133c5c77713c7a
Ashraf Uddin <ashrafunahid@gmail.com>
2021-10-05 12:22:28 +0600

    Created Even Odd Number Finding

---
+++ bin/evenOdd.dart
@@ -0,0 +1,12 @@
+void main() {
+  for(int i = 0; i <= 10; i++){
+   if(i % 2 == 0){
+     if(i != 0){
+      print('$i is a even number');
+     }
+   }
+    else{
+     print('$i is a odd number');
+    }
+ }
+}
