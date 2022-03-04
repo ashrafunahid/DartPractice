963ce240ac0671a75cf500b5184b989c8613c2d7
Ashraf Uddin <ashrafunahid@gmail.com>
2022-03-04 18:37:28 +0600

    .

--- bin/practice.dart
+++ bin/practice.dart
@@ -1,107 +1,107 @@
 import 'dart:io';
+
 // import 'package:intl/intl.dart';
 void main() {
-    // // For Loop
-    // int i;
-    // for(i=1; i<=5; i++){
-    //     print('Hello World ${i}');
-    // }
+  // // For Loop
+  // int i;
+  // for(i=1; i<=5; i++){
+  //     print('Hello World ${i}');
+  // }
 
-    // // While Loop
-    // int i=0;
-    // while(i<5){
-    //     print('Hello World ${i}');
-    //     i++;
-    // }
+  // // While Loop
+  // int i=0;
+  // while(i<5){
+  //     print('Hello World ${i}');
+  //     i++;
+  // }
 
-    // // Do While
-    // int i = 0;
-    // do {
-    //     print('Hello World ${i+1}');
-    //     i++;
-    // }
-    // while (i < 5);
+  // // Do While
+  // int i = 0;
+  // do {
+  //     print('Hello World ${i+1}');
+  //     i++;
+  // }
+  // while (i < 5);
 
-    // var number1 = 10;
-    // var number2 = 20;
-    // var result;
-    // if(number1>number2) {
-    //     // result = number1+number2;
-    //     // result = number1-number2;
-    //     // result = number1*number2;
-    //     result = number1 / number2;
-    //     // result = number1%number2;
-    // }else{
-    //     // result = number2 + number1;
-    //     // result = number2 - number1;
-    //     // result = number2 * number1;
-    //     result = number2 / number1;
-    //     // result = number2 % number2;
-    // }
-    // print('Result is ${result}');
+  // var number1 = 10;
+  // var number2 = 20;
+  // var result;
+  // if(number1>number2) {
+  //     // result = number1+number2;
+  //     // result = number1-number2;
+  //     // result = number1*number2;
+  //     result = number1 / number2;
+  //     // result = number1%number2;
+  // }else{
+  //     // result = number2 + number1;
+  //     // result = number2 - number1;
+  //     // result = number2 * number1;
+  //     result = number2 / number1;
+  //     // result = number2 % number2;
+  // }
+  // print('Result is ${result}');
 
-    // Assignment Operator
+  // Assignment Operator
 
-    // int a = 10;
-    // a += 5;
-    // print(a);
+  // int a = 10;
+  // a += 5;
+  // print(a);
 
-    // var a = 10.0;
-    // a /= 5;
-    // print(a);
+  // var a = 10.0;
+  // a /= 5;
+  // print(a);
 
-    // var a = 10.0;
-    // a %= 3;
-    // print(a);
+  // var a = 10.0;
+  // a %= 3;
+  // print(a);
 
-    // Increament / Decrement
+  // Increament / Decrement
 
-    // var a = 10;
-    // print(++a);
-    // print(++a);
-    // print(a);
-    //
-    // var a = 10;
-    // print(a++);
-    // print(a++);
-    // print(a);
+  // var a = 10;
+  // print(++a);
+  // print(++a);
+  // print(a);
+  //
+  // var a = 10;
+  // print(a++);
+  // print(a++);
+  // print(a);
 
-    // var a = 10;
-    // print(--a);
-    // print(--a);
-    // print(a);
+  // var a = 10;
+  // print(--a);
+  // print(--a);
+  // print(a);
 
-    // var a = 10;
-    // print(a--);
-    // print(a--);
-    // print(a);
+  // var a = 10;
+  // print(a--);
+  // print(a--);
+  // print(a);
 
-    // var i = 10;
-    // for(i=1; i<=10; i++){
-    //   if(i%2 == 0){
-    //     print('Here ${i} is Even Number');
-    //   }else{
-    //     print('Here ${i} is Odd Number');
-    //   }
+  // var i = 10;
+  // for(i=1; i<=10; i++){
+  //   if(i%2 == 0){
+  //     print('Here ${i} is Even Number');
+  //   }else{
+  //     print('Here ${i} is Odd Number');
+  //   }
 
-    // Switch
+  // Switch
 
+  // switch(name){
+  //   case 'Ashraf':
+  //     print('This is Ashraf');
+  //     break;
+  //   case '2':
+  //     print('This is case 2');
+  //     break;
+  //   case '3':
+  //     print('This is case 3');
+  //     break;
+  //   default:
+  //     print('Invalid value or not matched with case');
+  //     break;
 
-    // switch(name){
-    //   case 'Ashraf':
-    //     print('This is Ashraf');
-    //     break;
-    //   case '2':
-    //     print('This is case 2');
-    //     break;
-    //   case '3':
-    //     print('This is case 3');
-    //     break;
-    //   default:
-    //     print('Invalid value or not matched with case');
-    //     break;
-
-    // }
+  // }
 
 // import 'package:intl/intl.dart';
 // void main() {
@@ -186,7 +186,74 @@ void main() {
 //
 // double division(double num1, double num2){
 //   return num1/num2;
+// import 'package:intl/intl.dart';
+
+//     DateTime date = DateTime.now();
+//     String dateFormat = DateFormat('EEEE').format(date);
+
+// String day = 'monday';
+// if (day == 'sunday'){
+//   print("Today is sunday");
+// }
+// else if (day == 'saturday'){
+//   print("Today is saturday");
+// }
+// else{
+//   print("Day is not listed");
+// }
+//
+// for(int i = 1; i<=10; i++){
+//   if(i%2 != 0){
+//     if(i == 3){
+//       continue;
+//     }
+//     else if(i == 7){
+//       break;
+//     }else {
+//       print("${i} is a Odd number");
+//     }
+//   }
+//   else{
+//     print("${i} is a Even Number");
+//   }
+// }
+
+// int i = 10;
+// do{
+//   print(i);
+//   i = i+2;
+// }
+// while(i<=20);
+
+// int sum, i, j;
+// sum = 0;
+// for(i = -1; i <= 10; i = i+2){
+//   j = i*i;
+//   if(i > 5){
+//     break;
+//   }
+//   sum = sum + (i+j);
+//   print("I = ${i}, J = ${j}, SUM = ${sum}");
+// }
+// print("Outside of Loop I = ${i}, SUM = ${sum}");
+
+// var myValues = new Set.from({"Rumon", "Nazrul", "Hasan", "Robin"});
+// var myKeys;
+//
+// for (myKeys in myValues){
+//   print("${myKeys}");
+// }
+
+// var add = Addition();
+// print(Addition());
+
 
 
 
 }
+
+// int Addition(){
+//   var a = 10, b = 20, result;
+//   result = a + b;
+//   return result;
+// }
